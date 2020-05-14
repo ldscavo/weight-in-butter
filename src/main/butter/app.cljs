73057831211d474d31801d0dab6da9167b3fa2d0
Logan Scavo <logan@scavo.dev>
2020-05-13 21:50:27 -0400

    Migrated project to shadow-cljs

---
+++ src/main/butter/app.cljs
@@ -0,0 +1,45 @@
+(ns butter.app)
+
+(def oz-in-butter 4)
+(def oz-in-lb 16)
+(def oz-in-kg 35.26)
+
+(defn to-oz [weight unit]
+  (* weight
+    (if (= unit "lbs")
+      oz-in-lb
+      oz-in-kg)))
+
+(defn oz-to-butter [ounces]
+  (/ ounces oz-in-butter))
+
+(defn to-butter [weight unit]
+  (oz-to-butter
+    (to-oz weight unit)))
+
+(defn output [content]
+  (set!
+    (.-innerHTML
+      (.getElementById js/document "results"))
+    content))
+
+(defn get-input-value [id]
+  (.-value
+    (.getElementById js/document id)))
+
+(defn weight []
+  (int
+    (get-input-value "weight")))
+
+(defn unit [] 
+  (get-input-value "units"))
+
+(defn calculate []
+  (output
+    (str "This weighs as much as "
+      (to-butter (weight) (unit))
+      " sticks of butter!")))
+
+(defn init []
+  (.addEventListener
+    (.getElementById js/document "calculate") "click" calculate))
