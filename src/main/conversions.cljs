734b48e0e9582b320f4cf3061f16a557bca0ca72
ldscavo <logan@scavo.dev>
2021-06-03 22:01:30 -0400

    Fixed the grams calculation

--- src/main/conversions.cljs
+++ src/main/conversions.cljs
@@ -4,6 +4,8 @@
 (def oz-in-lb 16)
 (def oz-in-kg 35.26)
 (def g-in-oz 28.35)
+(def g-in-lb 453.592)
+(def g-in-kg 1000)
 
 (defn validate [n]
   (if (< n 0) 0 n))
@@ -27,9 +29,13 @@
   (/ ounces oz-in-butter))
 
 (defn to-g [weight units]
-  (->> (to-oz weight units)
-       (oz-to-g)))
+  (if (= units "lbs")
+    (* weight g-in-lb)
+    (* weight g-in-kg)))
 
 (defn to-butter [weight units]
   (->> (to-oz weight units)
        (oz-to-butter)))
+
+(comment
+  (to-g 50 "kg"))
